dcd3d1a5534abe65af5c60c4327966043bea2374
yakovcohen4 <yakovc1431@gmail.com>
2021-09-20 19:01:26 +0300

    fix

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 
-const loadingEl = document.getElementById("loading");
 document.getElementById("button").addEventListener("click", ()=>{sentimentor()});
+const loadingEl = document.getElementById("loading");
+const catImgEl = document.createElement("img");
 
 async function sentimentor (){
+    try {
  
-    const url = "https://sentim-api.herokuapp.com/api/v1/";
-    const body = JSON.stringify({ "text": document.getElementById("text").value })
-    const headers = {  
-        Accept: "application/json",
-        "Content-Type": "application/json" 
-    }
-    console.log("body "+body)
-    console.log("headers "+headers)
-    loadingEl.hidden=false;
-    const respone = await fetch (url,{
-        method: "POST",
-        headers,
-        body
-    })
-    const data = await respone.json();
-    loadingEl.hidden=true;
-    console.log(data)
+        const url = "https://sentim-api.herokuapp.com/api/v1/";
+        const body = JSON.stringify({ "text": document.getElementById("text").value })
+        const headers = {  
+            Accept: "application/json",
+            "Content-Type": "application/json" 
+        }
 
-    document.getElementById("result").textContent = `The result is: ${data.result.type}`
-   
-    const color = document.getElementById("result");
-    if (data.result.type === 'positive'){
-        color.classList = "positive";
-    }
-    if (data.result.type === 'negative'){
-        color.classList = "negative";
-    }
-    if (data.result.type === 'neutral'){
-        color.classList = "neutral";
-    }
+        loadingEl.hidden=false;
+        const response = await fetch (url,{
+            method: "POST",
+            headers,
+            body
+        })
+        const data = await response.json();
+        console.log(data.error)
+        loadingEl.hidden=true;
+        
+        const result = data.result;
 
+        document.getElementById("result").textContent = `The result is: ${result.type},${result.polarity}`
+
+        const color = document.getElementById("result");
+        if (result.type === 'positive'){
+            color.classList = "positive";
+        }
+        if (result.type === 'negative'){
+            color.classList = "negative";
+        }
+        if (result.type === 'neutral'){
+            color.classList = "neutral";
+        }
+        console.log(response)
+        catImgEl.setAttribute("src", `https://http.cat/${response.status}`);
+        document.getElementById("catImg").append(catImgEl);
+        if (!response.ok){
+            throw response.status 
+        }
+
+    }
+    catch(error){
+        const errorEl= document.getElementById('mistake');
+        errorEl.textContent= 'You had a API problem ' + error;
+    }
 }
-
-
